75f7d26b50f0f3371e6397505befbcb1a15a9dc1
Aguntok <agun@localhost.localdomain>
2022-05-17 03:35:44 +0600

    update procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bbc_management.wsgi
--- bbc_management/settings.py
+++ bbc_management/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!_7u#gg#r2$ovc3ld(rvm*s(%e3h3b6yhhhwf60566d1s&#=+3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'bbcsbd.herokuapp.com']
 
 
 # Application definition
