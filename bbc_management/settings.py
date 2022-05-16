868ac8011c1e0a3eb27ce18428d838102afdc984
Aguntok <agun@localhost.localdomain>
2022-05-17 03:20:30 +0600

    update

--- bbc_management/settings.py
+++ bbc_management/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!_7u#gg#r2$ovc3ld(rvm*s(%e3h3b6yhhhwf60566d1s&#=+3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
